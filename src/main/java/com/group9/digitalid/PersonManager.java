2c3d43c27734e5436a5145fb633258b71adf325c
s4037298-ps1nov <s4037298@student.rmit.edu.au>
2026-02-17 15:23:00 +1100

    added updatePersonalDetails method

---
+++ src/main/java/com/group9/digitalid/PersonManager.java
@@ -0,0 +1,10 @@
+package com.group9.digitalid;
+
+public class PersonManager {
+    private static final String FILENAME = "citizens.txt";
+
+    public boolean updatePersonalDetails(String oldID, String newID, String newName, 
+                                        String newAddress, String newBirthdate) {
+        return false;
+    }
+}
