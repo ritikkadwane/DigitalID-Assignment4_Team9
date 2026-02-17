fcd55392efd838fe005789e0799e39c2e9dbab3e
vishnu21v <vishnuvaitheeswaran2002@gmail.com>
2026-02-16 23:09:23 -0800

    Setup PersonDemeritTest class with JUnit imports

---
+++ src/test/java/com/group9/digitalid/PersonTest.java
@@ -0,0 +1,8 @@
+// Import JUnit classes for assertions and tests
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+// Test class for Person demerit points functionality
+class PersonDemeritTest {
+   // Placeholder class, actual tests will be added later
+}
